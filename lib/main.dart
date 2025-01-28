dc1787d1e7a1b4527a3187234af113385b507fa6
kashinathpal <kashinathpal182@gmail.com>
2025-01-29 01:05:19 +0530

    This is my Flutter basic code.

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: const SplashScreen(),
-      debugShowCheckedModeBanner: false,  // Disable the debug banner
     );
   }
 }
